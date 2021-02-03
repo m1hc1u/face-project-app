d66e29059fd3633c788909373c149e37f3be5862
m1hc1u <jmmh@o2.pl>
2021-02-03 16:25:48 +0100

    Functional "Find out more" button scrolls to image below.

--- src/components/Moreinfo.js
+++ src/components/Moreinfo.js
@@ -1,21 +1,25 @@
-// import ScrollDown from 'ScrollDown';
-import React from 'react';
+import React, { useRef } from 'react'
 import ManyFaces from '../img/smiling-faces-happy-group.jpg';
 
 const Moreinfo = () => {
+    const imageRef = useRef();
+
+    function handleButtonClick () {
+        imageRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+
     return (
         <section className="container">
             <div className="encourage-to-know">
                 <h1>discover Facé</h1>
                 <p>Our algorithm will generate a Human according to the parameters you choose</p>
                 <div className="know-more">
-                    <button type="button" className="know-more--btn"
-                            onClick="smoothScroll(document.getElementById('many-faces'))">
+                    <button type="button" className="know-more--btn" onClick={handleButtonClick}>
                         Find out more
                     </button>
                 </div>
                 {/*// <!-- przewijanie w prawo automatycznie -->*/}
-                <img src={ManyFaces} alt="faces"
+                <img ref={imageRef} src={ManyFaces} alt="faces"
                      className="many-faces" id="many-faces" />
             </div>
         </section>
