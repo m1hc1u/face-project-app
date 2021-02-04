21f93c0e65c2dcf3b9ebbd4a756b2b1e2a4cbef0
m1hc1u <jmmh@o2.pl>
2021-02-04 14:25:29 +0100

    change of text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-container">
                 <p>Copyright by Facé 2021.
-                    Surreal are faces here.</p>
+                    Surreal art faces h're.</p>
             </div>
             <div className="social-medias">
                 <a href="http://www.facebook.com"><img className="sm" src={Facebook} /></a>
