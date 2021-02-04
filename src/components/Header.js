099a36b1bd500542f1809ef19600ceb650c90fe3
m1hc1u <jmmh@o2.pl>
2021-02-04 14:25:04 +0100

    added div 'logos-left' to attach logos together.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <header className='page-header'>
             <div className='header-full-menu'>
+                <div className='logos-left'>
                 <a href="/public/index.html">
                     <img className='face-logo' src={FaceLogo} alt='facé profile' />
                 </a>
@@ -12,6 +13,7 @@ const Header = () => {
                     <a href="/public/index.html">
                         <h1 className="logo-text">Facé</h1>
                     </a>
+                </div>
                 </div>
                 <nav>
                     <ul className="navi">
