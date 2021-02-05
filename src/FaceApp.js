cf3be2fdb5835965a96f4122d6830034bad291ea
m1hc1u <jmmh@o2.pl>
2021-02-05 18:34:52 +0100

    commented by Kuba

--- src/FaceApp.js
+++ src/FaceApp.js
@@ -1,3 +1,15 @@
+// //prosimy uzytkowanika o opcje
+//
+// fetch(`https://api.generated.photos/api/v1/?face_length=${face_length}`, {
+//     headers: {
+//         Authorization: 'GkWzZRe_DY7PteRqi_cyfA'
+//     }
+// })
+//     .then(res => res.json())
+//     .then(photos => {
+//         console.log(photos)
+//     });
+
 import React from 'react';
 import FaceLogo from "./img/face-logo.svg";
 
