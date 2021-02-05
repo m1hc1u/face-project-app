ff3aa00c93aca2ed29d52b0f346941281c53ffd8
m1hc1u <jmmh@o2.pl>
2021-02-05 18:18:33 +0100

    <b>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                <p>Copyright by Facé 2021.
+                <p>Copyright by <b>Facé</b> 2021.
                     Surreal art faces h're.</p>
             </div>
             <div className="social-medias">
-                <a href="http://www.facebook.com"><img className="sm" src={Facebook} /></a>
+                <a href="http://www.facebook.com"><img className="sm" src={Facebook} alt="fb"/></a>
                 <br />
-                    <a href="http://www.twitter.com"><img className="sm" src={Twitter} /></a>
+                    <a href="http://www.twitter.com"><img className="sm" src={Twitter} alt="twit"/></a>
             </div>
         </footer>
     );
