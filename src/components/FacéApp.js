415856a22886a46c35536902248472111f0ef3cd
m1hc1u <jmmh@o2.pl>
2021-02-05 18:39:14 +0100

    Added FaceApp component and changed back to App.js name with blank space.

---
+++ src/components/FacéApp.js
@@ -0,0 +1,12 @@
+// //prosimy uzytkowanika o opcje
+//
+// fetch(`https://api.generated.photos/api/v1/?face_length=${face_length}`, {
+//     headers: {
+//         Authorization: 'GkWzZRe_DY7PteRqi_cyfA'
+//     }
+// })
+//     .then(res => res.json())
+//     .then(photos => {
+//         console.log(photos)
+//     });
+
