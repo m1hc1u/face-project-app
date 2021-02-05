177b5a61f2ec9371e2245bb335d5e8d1531cf781
m1hc1u <jmmh@o2.pl>
2021-02-05 18:21:13 +0100

    RegForm

--- src/components/Preheader.js
+++ src/components/Preheader.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 const Preheader = () => {
     return (
         <div className="pre-header-msg">
             <div className="pre-header-msg--txt">Do you want to use our app?</div>
             {/*// kliknij, aby otworzyć panel rejestracji*/}
-            <a className="pre-header-msg--btn" href="#">Register now</a>
+            <a className="pre-header-msg--btn h3" onClick={RegistrationForm}>Register now</a>
         </div>
     );
 }
