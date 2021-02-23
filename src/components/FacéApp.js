2f0f49d0a3cacb95845a815b7ec9f0423f20a893
m1hc1u <jmmh@o2.pl>
2021-02-23 20:50:45 +0100

    Added RandomFacé.js so it renders face if needed.

--- src/components/FacéApp.js
+++ src/components/FacéApp.js
@@ -1,23 +1,14 @@
-// //prosimy uzytkowanika o opcje
-//
-// fetch(`https://api.generated.photos/api/v1/?face_length=${face_length}`, {
-//     headers: {
-//         Authorization: 'GkWzZRe_DY7PteRqi_cyfA'
-//     }
-// })
-//     .then(res => res.json())
-//     .then(photos => {
-//         console.log(photos)
-//     });
-
 import React from 'react';
 import Header from "./Header";
+import RandomFacé from "./RandomFacé";
 
 const FacéApp = () => {
     return (
-        <Header />
-    //    tu właściwa apka i jej treść
-    )
+        <div>
+            <Header />
+            <RandomFacé />
+        </div>
+    );
 }
 
 export default FacéApp;
