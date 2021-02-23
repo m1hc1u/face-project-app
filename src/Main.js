0936d36f7f0e8b719f17cb2eb52e1bccd13ee9b1
m1hc1u <jmmh@o2.pl>
2021-02-23 21:02:18 +0100

    route to random-face

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ import RegistrationForm from './components/RegistrationForm';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Preheader from './components/Preheader';
 import Home from './components/Home';
+import FacéApp from "./components/FacéApp";
 
 function Main() {
   return (
@@ -11,15 +12,17 @@ function Main() {
             <Preheader />
                 <div className="container d-flex align-items-center flex-column">
                     <Switch>
-                        <Route path="/reg" exact={true}>
+                        <Route path="/registration" exact={true}>
                             <RegistrationForm />
                         </Route>
                         <Route path="/" exact={true}>
                             <Home />
                         </Route>
+                        <Route path="/random-face" exact={true}>
+                            <FacéApp />
+                        </Route>
                     </Switch>
                 </div>
-
         </div>
       </Router>
   );
