cad0db9499b40d57d16235bb747728b315a21462
m1hc1u <jmmh@o2.pl>
2021-02-25 14:59:38 +0100

    Added appropriate ifs for facé parameters. Console generates array, however no image is created.

--- src/components/RandomFacé.js
+++ src/components/RandomFacé.js
@@ -9,11 +9,19 @@ const RandomFacé = () => {
     const [age, setAge] = useState('');
     const [hairLength, setHairLength] = useState('');
 
-    const generateFace = () => {
+    const generateFace = (e) => {
+        e.preventDefault();
         let url = `https://api.generated.photos/api/v1/faces?per_page=1`;
         if(gender) {
-            url += `?gender=${gender}`
+            url += `&gender=${gender}`
         }
+        if(age) {
+            url +=`&age=${age}`
+        }
+        if(hairLength) {
+            url +=`&hair_length=${hairLength}`
+        }
+
         fetch(url, {
             headers: {
                 Authorization: 'API-Key GkWzZRe_DY7PteRqi_cyfA'
@@ -22,40 +30,38 @@ const RandomFacé = () => {
             .then(res => res.json())
             .then(photos => {
                 console.log(photos);
-                console.log(photos.faces[0].urls);
-                setFace(photos.faces[0].urls[4]['512'])
+                if(photos.length) {
+                    setFace(photos.faces[0].urls[4]['512'])
+                }
             })
     }
     return (
        <div>
-           <form method="post" action="">
-               <select name="gender" required>
+           <form method="post" action="" onSubmit={generateFace}>
+               <select name="gender" value={gender} onChange={(e) => setGender(e.target.value)} required>
+                   <option value="">Gender</option>
                    <option value="male">Male</option>
                    <option value="female">Female</option>
                </select>
-               <select name="age" required>
+               <select name="age" value={age} onChange={(e) => setAge(e.target.value)} required>
+                   <option value="">Age</option>
                    <option value="infant">Infant</option>
                    <option value="child">Child</option>
                    <option value="young-adult">Young-adult</option>
                    <option value="adult">Adult</option>
                    <option value="elderly">Elderly</option>
                </select>
-               <select name="hair_length" required>
+               <select name="hair_length" value={hairLength} onChange={(e) => setHairLength(e.target.value)}  required>
+                   <option value="">Hair length</option>
                    <option value="short">Short</option>
                    <option value="medium">Medium</option>
                    <option value="long">Long</option>
                </select>
-               <input type="submit" value="submit" />
+               <button type='submit'>Generate Facé</button>
            </form>
-           <button onClick={() => generateFace()}>Generate Facé</button>
            {!face ? <p>Loading...</p> : <img src={face}/>}
        </div>
     )
 }
 
 export default RandomFacé;
-
-// stworz form
-//popros uzytkownika o info na temat czlowieka
-// zmontuj na podstawie tych danych url - if usr +=
-//po kliku na generate wyswietl zdjecie
